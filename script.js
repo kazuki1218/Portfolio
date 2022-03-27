0e9f64e3232ba54a5c2b14aa848cc0570b0f02cc
kazuki1218 <v.shiotakazuki.1218@gmail.com>
2022-03-27 19:50:59 +0900

    スクロール変更しないよー

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $.scrollify({
     section: ".box", //1ページスクロールさせたいエリアクラス名
     scrollbars: "false", //スクロールバー表示・非表示設定
     interstitialSection: "#header,#footer", //ヘッダーフッターを認識し、1ページスクロールさせず表示されるように設定
-    easing: "swing", // 他にもlinearやeaseOutExpoといったjQueryのeasing指定可能
+    easing: "linear", // 他にもlinearやeaseOutExpoといったjQueryのeasing指定可能
     scrollSpeed: 1000, // スクロール時の速度
 
     //以下、ページネーション設定
